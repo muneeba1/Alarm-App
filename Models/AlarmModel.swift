193bfa5faa109940fd7af8595fef44a865eb89ec
Asher <asher.rosenblatt@me.com>
2017-07-07 14:17:35 -0500

    working on alarms

--- Models/AlarmModel.swift
+++ Models/AlarmModel.swift
@@ -11,15 +11,16 @@ import MediaPlayer
 
 class AlarmModel: NSObject
 {
-    enum weekdays { case Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+    let ud: UserDefaults = UserDefaults.standard
+    enum Weekdays : Int{ case Monday = 0, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
 
     var date: Date = Date()
     var enabled: Bool = false
-    var weekDay: weekdays
+    var weekDay: Weekdays
     var label: String = "Alarm"
     var onSnooze: Bool = false
     
-    init(date:Date, enabled:Bool, weekDay: weekdays, label:String)
+    init(date:Date, enabled:Bool, weekDay: Weekdays, label:String)
     {
         self.date = date
         self.weekDay = weekDay
@@ -29,7 +30,7 @@ class AlarmModel: NSObject
         super.init()
     }
     
-    init(date:Date, enabled:Bool, weekDay: weekdays, label:String, onSnooze: Bool)
+    init(date:Date, enabled:Bool, weekDay: Weekdays, label:String, onSnooze: Bool)
     {
         self.date = date
         self.weekDay = weekDay
@@ -38,15 +39,37 @@ class AlarmModel: NSObject
         self.onSnooze = onSnooze
         super.init()
     }
+    
+    func save()
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("hh:mm a")
+        ud.set(dateFormatter.string(from: date), forKey:"\(weekDay)alarmTime")
+        ud.set(enabled, forKey:"\(weekDay)alarmEnabled")
+        ud.set(label, forKey: "\(weekDay)alarmLabel")
+        ud.synchronize()
+    }
+    
+    class func getAlarms() -> [AlarmModel]
+    {
+        let ud: UserDefaults = UserDefaults.standard
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("hh:mm a")
+        var alarms: [AlarmModel] = []
+        for i in 0...6
+        {
+            let dateString = ud.object(forKey: "\(i)alarmTime") as? String ?? "07:00 AM"
+            let date = dateFormatter.date(from: dateString) ?? Date()
+            let enabled = ud.object(forKey: "\(i)alarmEnabled") as? Bool ?? false
+            let label = ud.object(forKey: "\(i)alarmLabel") as? String ?? "Alarm"
+            let alarm: AlarmModel = AlarmModel(date: date, enabled: enabled, weekDay: Weekdays(rawValue: i)!, label: label)
+            alarms.append(alarm)
+        }
+        return alarms
+    }
 }
 extension AlarmModel
 {
-    var formattedTime: String
-    {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "h:mm a"
-        return dateFormatter.string(from: self.date)
-    }
     
     
 }
--- View Controllers/HomeViewController.swift
+++ View Controllers/HomeViewController.swift
@@ -7,31 +7,74 @@
 //
 
 import UIKit
+import UserNotifications
 
-class HomeViewController: UIViewController {
-  
-    @IBOutlet weak var tableView: UITableView!
+class HomeViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var homeTableView: UITableView!
+    let scheduler = AlarmService()
+    var alarms: [AlarmModel] = []
+    var today: Int = 0
+
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let notificationCenter = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.alert, .sound]
+        notificationCenter.getNotificationSettings { (settings) in
+            if settings.authorizationStatus != .authorized
+            {
+                notificationCenter.requestAuthorization(options: options, completionHandler: { (success, error) in
+                    print("notifications success \(success) and error \(error!.localizedDescription)")
+                })
+            }
+            else
+            {
+                print("already authorized")
+                let date = Date().addingTimeInterval(TimeInterval(exactly: 60)!)
+                let alarm = AlarmModel(date: date, enabled: true, weekDay: .Thursday, label: "Testing 1")
+                self.scheduler.setupNotificationsForAlarm(alarm: alarm)
+            }
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("EEEE MMMMd")
+        self.title = dateFormatter.string(from: Date())
+        let cal :Calendar = Calendar(identifier: .gregorian)
+        let weekDay = cal.component(.weekday, from: Date())
+        today = weekDay
+    }
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        alarms = AlarmModel.getAlarms()
+        print("test")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        if indexPath.section == 0
+        {
+            let alarmCell = tableView.dequeueReusableCell(withIdentifier: "alarmCell", for: indexPath) as! AlarmTableViewCell
+            return alarmCell
+        }
+        else
+        {
+            let infoCell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath) as! InfoTableViewCell
+            return infoCell
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 0
     }
-    */
-
 }
